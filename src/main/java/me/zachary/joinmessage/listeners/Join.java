6932c92ae4caac6d3a861efcd344c3e4987353d7
Zach_FR <zac232003@icloud.com>
2020-11-06 17:35:45 -0500

    Code cleanup.

--- src/main/java/me/zachary/joinmessage/listeners/Join.java
+++ src/main/java/me/zachary/joinmessage/listeners/Join.java
@@ -1,7 +1,6 @@
 package me.zachary.joinmessage.listeners;
 
 import me.zachary.joinmessage.JoinMessage;
-import me.zachary.joinmessage.utils.ChatColorHex;
 import me.zachary.joinmessage.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -22,80 +21,42 @@ public class Join implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
         Player p = e.getPlayer();
-        if (xyz.theprogramsrc.supercoreapi.spigot.utils.ReflectionUtils.VERSION.contains("1_16")) {
-            if (plugin.getConfig().getBoolean("Join_enable")){
-                if (p.hasPlayedBefore()){
-                    e.setJoinMessage(Utils.chat(ChatColorHex.parseHexColors(plugin.getConfig().getString("Join_message").replace("<player>", p.getName()))));
-                }else {
-                    if (plugin.getConfig().getBoolean("Welcome_message_enable")) {
-                        e.setJoinMessage(Utils.chat(ChatColorHex.parseHexColors(plugin.getConfig().getString("Welcome_message").replace("<player>", p.getName()))));
-                    } else {
-                        e.setJoinMessage("");
-                    }
-                }
-            } else if(!p.hasPlayedBefore()){
+        if (plugin.getConfig().getBoolean("Join_enable")){
+            if (p.hasPlayedBefore()){
+                e.setJoinMessage(Utils.color(plugin.getConfig().getString("Join_message").replace("<player>", p.getName())));
+            }else {
                 if (plugin.getConfig().getBoolean("Welcome_message_enable")) {
-                    if (!p.hasPlayedBefore()) {
-                        e.setJoinMessage(Utils.chat(ChatColorHex.parseHexColors(plugin.getConfig().getString("Welcome_message").replace("<player>", p.getName()))));
-                    }else{
-                        e.setJoinMessage("");
-                    }
-                }else {
+                    e.setJoinMessage(Utils.color(plugin.getConfig().getString("Welcome_message").replace("<player>", p.getName())));
+                } else {
                     e.setJoinMessage("");
                 }
-            } else {
-                e.setJoinMessage("");
             }
-        }else {
-            if (plugin.getConfig().getBoolean("Join_enable")){
-                if (p.hasPlayedBefore()){
-                    e.setJoinMessage(Utils.chat(plugin.getConfig().getString("Join_message").replace("<player>", p.getName())));
-                }else {
-                    if (plugin.getConfig().getBoolean("Welcome_message_enable")) {
-                        e.setJoinMessage(Utils.chat(plugin.getConfig().getString("Welcome_message").replace("<player>", p.getName())));
-                    } else {
-                        e.setJoinMessage("");
-                    }
-                }
-            } else if(!p.hasPlayedBefore()){
-                if (plugin.getConfig().getBoolean("Welcome_message_enable")) {
-                    if (!p.hasPlayedBefore()) {
-                        e.setJoinMessage(Utils.chat(plugin.getConfig().getString("Welcome_message").replace("<player>", p.getName())));
-                    }else{
-                        e.setJoinMessage("");
-                    }
-                }else {
+        } else if(!p.hasPlayedBefore()){
+            if (plugin.getConfig().getBoolean("Welcome_message_enable")) {
+                if (!p.hasPlayedBefore()) {
+                    e.setJoinMessage(Utils.color(plugin.getConfig().getString("Welcome_message").replace("<player>", p.getName())));
+                }else{
                     e.setJoinMessage("");
                 }
-            } else {
+            }else {
                 e.setJoinMessage("");
             }
+        } else {
+            e.setJoinMessage("");
         }
     }
 
     @EventHandler
     public void onJoin(PlayerQuitEvent e){
         Player p = e.getPlayer();
-        if (xyz.theprogramsrc.supercoreapi.spigot.utils.ReflectionUtils.VERSION.contains("1_16")) {
-            if (plugin.getConfig().getBoolean("Leave_enable")){
-                if (p.hasPlayedBefore()){
-                    e.setQuitMessage(ChatColorHex.parseHexColors(plugin.getConfig().getString("Leave_message").replace("<player>", p.getName())));
-                }else {
-                    e.setQuitMessage(ChatColorHex.parseHexColors(plugin.getConfig().getString("Leave_message").replace("<player>", p.getName())));
-                }
+        if (plugin.getConfig().getBoolean("Leave_enable")){
+            if (p.hasPlayedBefore()){
+                e.setQuitMessage(Utils.color(plugin.getConfig().getString("Leave_message").replace("<player>", p.getName())));
             }else {
-                e.setQuitMessage("");
+                e.setQuitMessage(Utils.color(plugin.getConfig().getString("Leave_message").replace("<player>", p.getName())));
             }
-        } else {
-            if (plugin.getConfig().getBoolean("Leave_enable")){
-                if (p.hasPlayedBefore()){
-                    e.setQuitMessage(Utils.chat(plugin.getConfig().getString("Leave_message").replace("<player>", p.getName())));
-                }else {
-                    e.setQuitMessage(Utils.chat(plugin.getConfig().getString("Leave_message").replace("<player>", p.getName())));
-                }
-            }else {
-                e.setQuitMessage("");
-            }
+        }else {
+            e.setQuitMessage("");
         }
     }
 }
--- src/main/java/me/zachary/joinmessage/utils/Utils.java
+++ src/main/java/me/zachary/joinmessage/utils/Utils.java
@@ -2,10 +2,41 @@ package me.zachary.joinmessage.utils;
 
 import org.bukkit.ChatColor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utils {
 
     public static String chat (String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    private static final Pattern regex = Pattern.compile("&#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}).*?");
+
+    public static String parseHexColors(String message) {
+        final Matcher matcher = regex.matcher(message);
+        while (matcher.find()) {
+            final String color = matcher.group();
+            final String hexcolor = color.substring(1);
+            net.md_5.bungee.api.ChatColor c = null;
+            try {
+                c = net.md_5.bungee.api.ChatColor.of(hexcolor);
+            } catch (final Exception ignored) {
+            }
+            if (c != null) {
+                message = message.replaceAll(color, c.toString());
+            }
+        }
+        return message;
+    }
+
+    public static String color(String message){
+        if(xyz.theprogramsrc.supercoreapi.spigot.utils.ReflectionUtils.VERSION.contains("1_16")){
+            message = Utils.parseHexColors(Utils.chat(message));
+        }else{
+            message = Utils.chat(message);
+        }
+        return message;
     }
 
 }
