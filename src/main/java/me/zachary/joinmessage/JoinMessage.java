bcfa5e6b4eb25ed05fd5824c3028b785db95ff69
Zach_FR <zac232003@icloud.com>
2020-11-15 14:10:15 -0500

    Added ZachCore support

--- src/main/java/me/zachary/joinmessage/JoinMessage.java
+++ src/main/java/me/zachary/joinmessage/JoinMessage.java
@@ -2,10 +2,10 @@ package me.zachary.joinmessage;
 
 import me.zachary.joinmessage.commands.ReloadCommand;
 import me.zachary.joinmessage.listeners.Join;
+import me.zachary.zachcore.ZachCorePlugin;
 import me.zachary.zachcore.utils.Metrics;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class JoinMessage extends JavaPlugin {
+public final class JoinMessage extends ZachCorePlugin {
 
     @Override
     public void onEnable() {
@@ -17,6 +17,7 @@ public final class JoinMessage extends JavaPlugin {
         int pluginId = 7870; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
 
+        preEnable();
     }
 }
 
